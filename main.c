25f7f37ce2bd0b478f000bde95bacc2544e2ed39
Ruttapon-sup <156408502+Ruttapon-sup@users.noreply.github.com>
2024-02-23 08:04:12 +0000

    weekk7

--- main.c
+++ main.c
@@ -40,6 +40,7 @@ int main(int argc , char **argv) {
  for(i=1;i<argc;i++){
         if(strcmp(argv[i],"x")==0){
             x=dequeue_struct(&q);
+            if(x != 0)
             printf("dequeing %d\n",x);
         }
         else {
